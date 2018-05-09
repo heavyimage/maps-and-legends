c4e0d6dc806ffd680205041aa85b9d32d0148210
Jesse Spielman <jesse.spielman@gmail.com>
2018-05-09 03:05:14 -0400

    Cause script to bomb out in the event of multiple places with the same name colliding
    I'll fix this later but for now it'll stop people from accidentally clobbering their data!

--- bin/parse_kmz.py
+++ bin/parse_kmz.py
@@ -35,8 +35,7 @@ class Place(object):
     def __str__(self):
         return unicode(self).encode('utf-8')
 
-    def bake(self):
-
+    def get_output_path(self):
         output_dir = os.path.join(os.getcwd(), "_waypoints")
         if not os.path.exists(output_dir):
             raise RuntimeError("Can't find _waypoints dir; re-run from jekyll "
@@ -44,7 +43,11 @@ class Place(object):
 
         base = ''.join(ch.lower() for ch in self.name if ch.isalnum())
         filename = "%s.md" % base
-        output_path = os.path.join(output_dir, filename)
+        return os.path.join(output_dir, filename)
+
+    def bake(self):
+
+        output_path = self.get_output_path()
 
         lines = [u'---\n',
                  u'title: "%s"\n' % self.name,
@@ -61,11 +64,25 @@ kmz = ZipFile(sys.argv[1], 'r')
 doc = parser.parse(kmz.open('doc.kml', 'r')).getroot()
 folders = doc.findall('.//{http://www.opengis.net/kml/2.2}Folder')
 
-
-i = 0
+# Build places
+places = []
 for folder in folders:
     print folder.name
     for pm in folder.Placemark:
-        i+=1
-        place = Place(pm, folder.name)
-        place.bake()
+        places.append(Place(pm, folder.name))
+
+# Check that there aren't any collisions
+collisions = []
+paths = [place.get_output_path() for place in places]
+for place in places:
+    my_path = place.get_output_path()
+    if paths.count(my_path) > 1:
+        collisions.append(place)
+
+collisions = sorted(["%s (%s)" % (place.name, place.coords) for place in collisions])
+if len(collisions):
+    raise RuntimeError("Can't proceed -- collision between output files!  I probably should have a better naming approach...right?"
+                       "\n\t%s" % ("\n\t".join(collisions)))
+
+for place in places:
+    place.bake()
