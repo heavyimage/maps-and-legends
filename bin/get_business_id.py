c0dc03936fe7e46953e6ed0166ddb8d4aec616df
Jesse Spielman <jesse.spielman@gmail.com>
2018-05-11 20:46:48 -0400

    Add improvements to get_business_id

--- bin/get_business_id.py
+++ bin/get_business_id.py
@@ -4,8 +4,10 @@ import requests
 import pickle
 import pprint
 
-URL = 'https://api.foursquare.com/v2/venues/explore'
+VENUE_EXPLORE_URL = 'https://api.foursquare.com/v2/venues/explore'
+VENUE_DETAILS_URL = 'https://api.foursquare.com/v2/venues/%s'
 CACHE = "output.dat"
+CACHE2 = "output2.dat"
 
 params = dict(
   client_id='XYU3UGC54SLPDLBXECKIQRJNYPGUVGGAPY0QDYLATSHUHSDW',
@@ -19,16 +21,54 @@ def explore_group(group):
     #pprint.pprint(group)
     for item in group['items']:
         print "Exact match:", item['flags']['exactMatch']
-        print "Categories:", [cat['shortName'] for cat in item['venue']['categories']]
         print "Foursquare Id:", item['venue']['id']
-        print "Foursquare Venue Page:", item['venue']['venuePage']['id']
+        #print "Foursquare Venue Page:", item['venue']['venuePage']['id']
         print "Address:", " ".join(item['venue']['location']['formattedAddress'])
         print "Photos:", item['venue']['photos']
         print "Exact match:", item['flags']['exactMatch'] == True
 
+        print "\n\n\n*** Details ***\n\n\n"
+
+        url = VENUE_DETAILS_URL % item['venue']['id']
+        if not os.path.exists(CACHE2):
+            resp = requests.get(url=url, params=params)
+            data = json.loads(resp.text)
+
+            with open(CACHE2, "w") as f:
+                pickle.dump(data, f)
+
+        with open(CACHE2, "r") as f:
+            data = pickle.load(f)
+        #pprint.pprint(data)
+        groups = data['response']['venue']['attributes']['groups']
+        for group in groups:
+            print group['name']
+            #pprint.pprint(group)
+
+        menus = [g for g in groups if g['name'] == "Menus"]
+
+        print "\n\n\n\n"
+        best_photo = data['response']['venue']['bestPhoto']
+        print "Photo:", best_photo
+        categories = data['response']['venue']['categories']
+        print "Categories:", [cat['shortName'] for cat in categories]
+        phone = data['response']['venue']['contact']['formattedPhone']
+        print "Phone:", phone
+        hours = data['response']['venue']['hours']['timeframes']
+        print "Hours:", hours
+        menu = data['response']['venue']['menu']['url']
+        print "Menu URL", menu
+
+        price = data['response']['venue']['price']
+        print "Price:", price
+        rating = data['response']['venue']['rating']
+        rating_signals = data['response']['venue']['ratingSignals']
+        print "Rating:", rating, "based on", rating_signals, "reviews"
+        url = data['response']['venue']['url']
+        print "URL", url
 
 if not os.path.exists(CACHE):
-    resp = requests.get(url=URL, params=params)
+    resp = requests.get(url=VENUE_EXPLORE_URL, params=params)
     data = json.loads(resp.text)
 
     with open(CACHE, "w") as f:
